c2c3a4cf9810f3ff3400d38524c617d3a7778e73
Fernanda <fegolim@hotmail.com>
2023-09-13 09:24:54 -0300

    add+

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+app.listen(8081, function(){
+    console.log("Conectado com sucesso!")
+})
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize("esp32", "root", "",{
+    host: "localhost",
+    dialect: "mysql"
+})
+sequelize.authenticate().then(function(){
+    console.log("Conectado com sucesso!")
+}).catch(function(erro){
+    console.log("Falha ao conectar: " + erro)
+})
+const Sensor = sequelize.define("sensors",{
+    temperatura:{
+        type: Sequelize.INTEGER
+    }
+})
+//Sensor.sync({force: true})
+app.get("/",function(req, res){
+    res.send("Projeto Esp")
+})
+app.get("/cadastrar/:item",function(req, res){
+    Sensor.create({
+        temperatura: req.params.item,
+    }).then(function(){
+        console.log("Cadastrado com sucesso!")
+    }).catch(function(erro){
+        console.log("Falha ao cadastrar os dados: " + erro)
+    })
+    res.redirect("/")
+})
